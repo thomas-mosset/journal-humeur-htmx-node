e2391810ce3e09de26f02e2d5a90c42f533ebd3f
Thomas <mosset.thomas@gmail.com>
2025-03-21 16:33:42 +0100

    feat: form + post request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const db = require("./config/database");
+import db from "./config/database.js";
 
 import createHomepage from './views/index.js';
 
@@ -16,6 +16,30 @@ app.get('/', (req, res) => {
     res.send(createHomepage());
 });
 
+app.post('/moods', (req, res) => {
+    const { date, mood, comment } = req.body;
+
+    if (!date || !mood) {
+        return res.status(400).send("Veuillez remplir tous les champs obligatoires.");
+    }
+
+    const query = `INSERT INTO moods (date, mood, comment) VALUES (?, ?, ?)`;
+
+    db.run(query, [date, mood, comment || null], function (err) {
+        if (err) {
+            return res.status(500).send("Erreur lors de l'ajout de l'humeur");
+        }
+
+        // res.redirect('/');
+
+        res.send(`
+            <li>
+                <strong>${date}</strong> - ${mood} <br>
+                ${comment ? `<em>${comment}</em>` : ""}
+            </li>
+        `);
+    })
+});
 
 // listen to port
 app.listen(3000, () => {
--- views/index.js
+++ views/index.js
@@ -2,6 +2,8 @@ const createHomepage = () => /*html*/`
     <!DOCTYPE html>
     <html>
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Journal d'humeur</title>
             <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
             <link rel="stylesheet" href="/styles.css" />
@@ -13,8 +15,34 @@ const createHomepage = () => /*html*/`
             </header>
 
             <main>
-                <div>
-                    <p>hello</p>
+                <div class="form">
+                <h2>Quelle est ton humeur aujourd'hui ?</h2>
+                    <form hx-post="/moods" hx-target="#moods-list" hx-swap="beforeend">
+                        <div>
+                            <label for="date">Entrez une date (format JJ-MM-AAAA)</label>
+                            <input type="text" id="date" name="date" placeholder="Ex: 21-03-2025" required />
+                        </div>
+
+                        <div>
+                            <label for="mood">Entrez votre humeur du jour (format émoji)</label>
+                            <input type="text" id="mood" name="mood" placeholder="Ex: 😊, 😡, 😢..." required />
+                        </div>
+
+                        <div>
+                            <label for="comment">Entrez un commentaire (optionnel)</label>
+                            <textarea name="comment" id="comment" placeholder="..."></textarea>
+                        </div>
+                        <div>
+                            <button type="submit">Ajouter mon humeur</button>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="moods">
+                    <h2>Historique des humeurs</h2>
+                    <ul id="moods-list">
+                        <!-- Les humeurs ajoutées apparaîtront ici via HTMX -->
+                    </ul>
                 </div>
             </main>
         </body>
