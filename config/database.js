fed2b1f825550339ce5a2de2a65d13ceeae1bf1d
Thomas <mosset.thomas@gmail.com>
2025-03-21 14:25:15 +0100

    DB : check if table is already created

--- config/database.js
+++ config/database.js
@@ -11,22 +11,27 @@ const db = new sqlite3.Database("./journal_humeur.db", (err) => {
     }
 });
 
-// Création de la table des humeurs
-db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS moods (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date VARCHAR(10) NOT NULL UNIQUE,
-            mood VARCHAR(50) NOT NULL,
-            comment TEXT NULL
-        )
-    `, (err) => {
-        if (err) {
-            console.error("Erreur lors de la création de la table :", err.message);
-        } else {
-            console.log("Table 'moods'créée.");
-        }
-    });
+// Vérifier si la table 'moods' existe avant de la créer
+db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='moods'", (err, row) => {
+    // Création de la table des humeurs si elle n'existe pas
+    if (!row) {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS moods (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date VARCHAR(10) NOT NULL UNIQUE,
+                mood VARCHAR(50) NOT NULL,
+                comment TEXT NULL
+            )
+        `, (err) => {
+            if (err) {
+                console.error("Erreur lors de la création de la table :", err.message);
+            } else {
+                console.log("Table 'moods'créée.");
+            }
+        });
+    } else {
+        console.log("Table 'moods' déjà existante, pas de nouvelle création de celle-ci.");
+    }
 });
 
 export default db;
